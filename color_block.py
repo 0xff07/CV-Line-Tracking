ccef4c79d36180da401c8098a29698bf76f482d6
f <f@fde-MacBook-Pro.local>
2018-04-09 13:53:37 +0800

    First commit

---
+++ color_block.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+lowerBound=np.array([0,0,0])
+upperBound=np.array([180,255,100])
+
+cam= cv2.VideoCapture(0)
+kernelOpen=np.ones((5,5))
+kernelClose=np.ones((20,20))
+
+
+while True:
+    ret, img=cam.read()
+    img=cv2.resize(img,(340,220))
+
+    #convert BGR to HSV
+    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    # create the Mask
+    mask=cv2.inRange(imgHSV,lowerBound,upperBound)
+    #morphology
+    maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+    maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+    maskFinal=maskClose
+    _, conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(img,conts,-1,(255,0,0),3)
+    if(conts):
+        x,y,w,h=cv2.boundingRect(conts[0])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255), 2)
+        if x < 130:
+            print "turn right"
+        elif x > 170:
+            print "turn left"
+        else:
+            print "center"
+
+    cv2.imshow("maskClose",mask)
+    #cv2.imshow("maskOpen",maskOpen)
+    #cv2.imshow("mask",mask)
+    cv2.imshow("cam",img)
+    cv2.waitKey(10)
