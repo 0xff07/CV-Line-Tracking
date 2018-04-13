061eed6e84a242368dda0b73bb5f77722e1079f7
f <f@fde-MacBook-Pro.local>
2018-04-13 17:48:49 +0800

    Slice image to make more versitile controlling

--- color_block.py
+++ color_block.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import math
 
+SLICE_NUM = 2
 lowerBound=np.array([0,0,0])
 upperBound=np.array([180,255,100])
 
@@ -10,35 +11,49 @@ kernelOpen=np.ones((5,5))
 kernelClose=np.ones((20,20))
 
 
+
+
 while True:
     ret, img=cam.read()
     img=cv2.resize(img,(340,220))
 
+    height, width = img.shape[:2]
+    cropped_top = img[int(0):int(height * 0.5), int(0):int(width)]
+    cropped_bot = img[int(height * 0.5):int(height), int(0):int(width)]
+    image = [cropped_top, cropped_bot]
+    seg_x = [0, 0]
+    seg_y = [0, 0]
+    seg_w = [0, 0]
+    seg_h = [0, 0]
+
+    #cv2.imshow("top",image[0])    
+    #cv2.imshow("bot",image[1])    
+
+    for i in range(0,SLICE_NUM) :
+
     #convert BGR to HSV
-    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        imgHSV= cv2.cvtColor(image[i],cv2.COLOR_BGR2HSV)
     # create the Mask
-    mask=cv2.inRange(imgHSV,lowerBound,upperBound)
+        mask=cv2.inRange(imgHSV,lowerBound,upperBound)
     #morphology
-    maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
-    maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
-    maskFinal=maskClose
-    _, conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(img,conts,-1,(255,0,0),3)
-    if(conts):
-        x,y,w,h=cv2.boundingRect(conts[0])
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255), 2)
-        # Do control scheme wrt xywh
-        
+        maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+        maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+        maskFinal=maskClose
+        _, conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(image[i],conts,-1,(255,0,0),3)
+        if(conts):
+            x,y,w,h=cv2.boundingRect(conts[0])
+            x = seg_x[i]
+            y = seg_y[i]
+            w = seg_w[i]
+            h = seg_h[i]
+            cv2.rectangle(image[i],(x,y),(x+w,y+h),(0,0,255), 2)
+        cv2.imshow("maskClose" + str(i),maskFinal)
+        cv2.imshow("cam" + str(i),image[i])
 
-        if x < 130:
-            print "turn right"
-        elif x > 170:
-            print "turn left"
-        else:
-            print "center"
-
-    cv2.imshow("maskClose",mask)
+    # Do control with segs
+    
     #cv2.imshow("maskOpen",maskOpen)
     #cv2.imshow("mask",mask)
-    cv2.imshow("cam",img)
+    #cv2.imshow("cam",img)
     cv2.waitKey(10)
