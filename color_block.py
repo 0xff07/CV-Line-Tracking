952a399bb990faf69633471346928b0a746120f0
0xff07 <0xff07@gmail.com>
2018-04-13 17:10:27 +0000

    Remove imshows for better performance

--- color_block.py
+++ color_block.py
@@ -24,10 +24,6 @@ while True:
     ret, img=cam.read()
     img=cv2.resize(img,(IMG_WIDTH,IMG_HEIGHT))
 
-    #cropped_top = img[int(0):int(IMG_HEIGHT / SLICE_NUM), int(0):int(IMG_WIDTH)]
-    #cropped_bot = img[int(IMG_HEIGHT / SLICE_NUM):int(IMG_HEIGHT), int(0):int(IMG_WIDTH)]
-    #image = [cropped_top, cropped_bot]
-
     for i in range(0,SLICE_NUM) :
         sliced_img[i] = img[X_DIV*i + 1:X_DIV*(i+1), int(0):int(IMG_WIDTH)]
         # convert BGR to HSV
@@ -47,15 +43,11 @@ while True:
             CM_w[i] = w
             CM_h[i] = h
             cv2.rectangle(sliced_img[i],(x,y),(x+w,y+h),(0,0,255), 2)
-        elif i > 0:
-            CM_x[i] = CM_x[i-1]
-            CM_y[i] = CM_y[i-1]
-            CM_w[i] = CM_w[i-1]
-            CM_h[i] = CM_h[i-1]
-            
     # Do control with segs
-    for i in range(0, SLICE_NUM - 1):
-        cv2.line(img, (CM_x[i], CM_y[i]), (CM_x[i+1], CM_y[i+1]), (0, 255, 0), 10)
-    cv2.imshow("cam",img)
-    
-    cv2.waitKey(10)
+    vec_t_x = CM_x[0] - CM_x[1]
+    vec_t_y = CM_y[0] - CM_y[1]
+    vec_t = [vec_t_x, vec_t_y]
+    angle = math.acos(-vec_t[1]/math.sqrt(vec_t[0]**2 + vec_t[1]**2))
+    angle = angle * 180 / math.pi
+    print vec_t
+    print angle
