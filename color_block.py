54f658c4f10cbc055f24d7b302205784bd994f3d
f <f@fde-MacBook-Pro.local>
2018-04-29 11:43:46 +0800

    Do slight generalizations.

--- color_block.py
+++ color_block.py
@@ -5,8 +5,8 @@ import Queue
 import time
 import RPi.GPIO as GPIO
 
-CAMERA_NO = 0
-SLICE_NUM = 2
+CAMERA_NO = 1
+SLICE_NUM = 32
 IMG_WIDTH = 360
 IMG_HEIGHT = 270
 QUEUE_SIZE = 200
@@ -23,12 +23,14 @@ CM_x = range(0, SLICE_NUM)
 CM_y = range(0, SLICE_NUM)
 CM_w = range(0, SLICE_NUM)
 CM_h = range(0, SLICE_NUM)
+LINE_POS = range(0, SLICE_NUM - 1)
+LINE_TANGENT = range(0, SLICE_NUM - 1)
 angles = range(0, SLICE_NUM - 1)
 
 sliced_img = range(0, SLICE_NUM)
 
 PID = [0, 0, 0]
-KPID = [50., 10./QUEUE_SIZE , 10000.] # Kp, Ki, Kd
+KPID = [1., 0./QUEUE_SIZE , 0.] # Kp, Ki, Kd
 GHB = Queue.Queue(QUEUE_SIZE);
 last = 0;
 
@@ -118,33 +120,31 @@ while True:
                         )
 
         # Calculate tangent angle
+        curve_cm = [0., 0.]
         for i in range(0, SLICE_NUM - 1):
-            pos = [CM_x[i]/2 + CM_x[i + 1]/2 - IMG_WIDTH/2, 
-                   CM_y[i]/2 + CM_y[i + 1]/2 - IMG_HEIGHT/2]
-            vec_t = [CM_x[i] - CM_x[i + 1], CM_y[i] - CM_y[i + 1]]
+            curve_cm[0] = curve_cm[0] + CM_x[i] - IMG_WIDTH/2.
+            curve_cm[1] += CM_y[i] + X_DIV * i
+            LINE_TANGENT[i] = [CM_x[i] - CM_x[i + 1], CM_y[i] - CM_y[i + 1]]
+            vec_t = LINE_TANGENT[i]
             rad = math.acos(vec_t[0]/math.sqrt(vec_t[0]**2 + vec_t[1]**2))
             angles[i] = rad * 180 / math.pi
 
+
+        curve_cm = [i / SLICE_NUM for i in curve_cm]
         # Calculate PID values
-        PID[0] = pos[0]
-        PID[2] = pos[0] - last
-        last = pos[0]
+        PID[0] = curve_cm[0]
+        PID[2] = curve_cm[0] - last
+        last = curve_cm[0]
 
         if not GHB.full():
-            GHB.put(pos[0])
-            PID[1] += pos[0]
+            GHB.put(curve_cm[0])
+            PID[1] += curve_cm[0]
         else:
-            PID[1] = PID[1] - GHB.get() + pos[0]
-            GHB.put(pos[0])
+            PID[1] = PID[1] - GHB.get() + curve_cm[0]
+            GHB.put(curve_cm[0])
 
-
-        ctrl_val = int(np.dot(
-                [PID[0], PID[1], PID[2]], 
-                [(1 - math.exp(-KPID[0]/100)), 1 - math.exp(-KPID[1]/100), 1 - math.exp(-KPID[2]/100)])/3.
-        )
+        ctrl_val = int(np.dot(PID, KPID)/sum(KPID))
         ctrl_val = np.interp(ctrl_val, [-150, 150], [SERVO_MID - SERVO_OFFSET, SERVO_MID +SERVO_OFFSET])
-        #ctrl_val = np.interp(ctrl_val, [-120, 150], [SERVO_MID - SERVO_OFFSET, SERVO_MID +SERVO_OFFSET])
-        #ctrl_val = np.interp(angles[0], [0., 180.], [SERVO_MID + SERVO_OFFSET, SERVO_MID - SERVO_OFFSET])
         print str(ctrl_val) 
         if __debug__:
             cv2.imshow("cam",img)
@@ -152,6 +152,8 @@ while True:
         print "ANGLE : " + str(angles[0])
         print "PID :" + str(PID)
         print "CONTROL :" + str(ctrl_val)
+        print "POS_CURV " 
+        print(curve_cm)
         servo_pwm.ChangeDutyCycle(ctrl_val)
         time.sleep(0.0001)
     except KeyboardInterrupt:
