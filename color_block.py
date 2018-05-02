cb9c67b4c9b83a0a08987bf0b61f66e857bdbd63
0xff07 <0xff07@gmail.com>
2018-05-02 09:27:03 +0000

    Test contours behavior

--- color_block.py
+++ color_block.py
@@ -61,25 +61,25 @@ class PID_controller():
 def extract_polygon(img, slice_num=16, LB=np.array([0,0,0]), UB=np.array([180,255,75])):
     IMG_HEIGHT, IMG_WIDTH,_ = img.shape
     X_DIV = int(IMG_HEIGHT/float(slice_num))
-    blur = cv2.GaussianBlur(img,(41,41),0)
-    cv2.imshow("img_blur", blur)
-    cv2.waitKey(10)
     kernelOpen = np.ones((5,5))
     kernelClose = np.ones((20,20))
     poly_points = [None] * slice_num
     detected_contours = [None] * slice_num
     
     for i in range(0, slice_num) :
-        sliced_img = blur[X_DIV*i + 1:X_DIV*(i+1), int(0):int(IMG_WIDTH)]
-        imgHSV = cv2.cvtColor(sliced_img,cv2.COLOR_BGR2HSV)
+        sliced_img = img[X_DIV*i + 1:X_DIV*(i+1), int(0):int(IMG_WIDTH)]
+        blur = cv2.GaussianBlur(sliced_img,(15,15),0)
+        imgHSV = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(imgHSV, LB, UB)
         maskOpen = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
         maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
         maskFinal = maskClose
         _,conts,_ = cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
+        conts = sorted(conts, key = cv2.contourArea)
         if(conts):
-            c = max(conts, key = cv2.contourArea)
+            c = conts[0]
+            
             M = cv2.moments(c)
             poly_points[i] = (int(M['m10']/M['m00']), int(M['m01']/M['m00']) + X_DIV * i)
             detected_contours[i] = c
