1686a99f68817592453e367f7d1aca03452a885f
0xff07 <0xff07@gmail.com>
2018-05-03 15:14:45 +0000

    Tune parameters

--- color_block.py
+++ color_block.py
@@ -11,7 +11,7 @@ ON_RPI = 1
 CAMERA_NO = 0
 IMG_WIDTH = 320
 IMG_HEIGHT = 240
-SERVO_MID = 7
+SERVO_MID = 8
 SERVO_OFFSET = 3
 SERVO_PIN = 12
 
@@ -60,7 +60,7 @@ class PID_controller():
         print "PID : " + str(self.PID)
         print "CONTROL : " + str(self.ctrl)
 
-def extract_polygon(img, slice_num=16, LB=np.array([0,0,0]), UB=np.array([180,255,75])):
+def extract_polygon(img, slice_num=16, LB=np.array([0,0,0]), UB=np.array([180,255,50])):
 
     IMG_HEIGHT, IMG_WIDTH,_ = img.shape
     X_DIV = int(IMG_HEIGHT/float(slice_num))
@@ -108,16 +108,32 @@ def evaluate_function(angle_part, translate_part, x, y):
     return math.exp(-1*y/IMG_HEIGHT/20.)*(
         (1 - 1.*x/IMG_WIDTH/2.0)*angle_part + 
          1.*x/IMG_WIDTH * translate_part)
+    #return angle_part
 
+def servo_test(pwm, SERVO_MID, SERVO_OFFSET):
+    while True:
+        pwm["SERVO"].ChangeDutyCycle(SERVO_MID)
+        time.sleep(1)
+        pwm["SERVO"].ChangeDutyCycle(SERVO_MID + SERVO_OFFSET)
+        time.sleep(1)
+        pwm["SERVO"].ChangeDutyCycle(SERVO_MID)
+        time.sleep(1)
+        pwm["SERVO"].ChangeDutyCycle(SERVO_MID - SERVO_OFFSET)
+        time.sleep(1)
+ 
 
 cam = cv2.VideoCapture(CAMERA_NO)
-controller = PID_controller([1000, 10, 150])
+controller = PID_controller([1000, 0, 350])
 if ON_RPI:
     pwm = PWM_init({"SERVO":12})
     os.system("python arduino_start.py")
 
+# 11.75 ~ 2
+
+SERVO_MID = 7.5
+SERVO_OFFSET = 4
+
 while True:
-    time.sleep(0.05)
     try:
         resolution = 16
         _, img = cam.read()
@@ -142,7 +158,7 @@ while True:
             ctrl_estimate = evaluate_function(angle_part, translate_part, curve_cm[0] - IMG_WIDTH/2.0, IMG_HEIGHT - curve_cm[1])
             controller.step(ctrl_estimate)
             ctrl = controller.get_ctrl()
-            servo_duty = np.interp(ctrl, [-90, 90], [3.7, 11])
+            servo_duty = np.interp(ctrl, [-90, 90], [SERVO_MID - SERVO_OFFSET, SERVO_MID + SERVO_OFFSET])
 
             if ON_RPI:
                 pwm["SERVO"].ChangeDutyCycle(servo_duty)
